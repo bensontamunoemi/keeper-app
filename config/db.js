5873f88cfe2be3427fece68fe97df11b8701f0a3
massive5566 <bensontamunoemi@gmail.com>
2021-03-28 02:27:52 +0400

    implimented register route backend  and jwt to receive web token on registration

--- config/db.js
+++ config/db.js
@@ -3,4 +3,19 @@ const mongoose = require('mongoose');
 
 const db = config.get('mongoURI');
 
-const connectDB = {};
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
