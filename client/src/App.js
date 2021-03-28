ac54470e636bd2272949a1a5b0424959437ac0f2
massive5566 <bensontamunoemi@gmail.com>
2021-03-28 20:57:53 +0400

    Worked on contact route and created client application folder with navbar integration

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+	return (
+		<Fragment>
+			<Navbar />
+		</Fragment>
+	);
+}
+
+export default App;
--- routes/contact.js
+++ routes/contact.js
@@ -23,21 +23,92 @@ router.get('/', authCheck, async (req, res) => {
 // @route POST api/contacts
 // @desc add new contact
 // @access Private
-router.post('/', (req, res) => {
-	res.send('Add new contact');
-});
+router.post(
+	'/',
+	[
+		authCheck,
+		[
+			check('name', 'Name is required').not().isEmpty(),
+			check('email', 'Email is required').isEmail(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, phone, type } = req.body;
+
+		try {
+			let newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id,
+			});
+			const contact = await newContact.save();
+			res.json(contact);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 // @route PUT api/contacts/:id
 // @desc update a contact
 // @access Private
-router.put('/:id', (req, res) => {
-	res.send('Update  contact');
+router.put('/:id', authCheck, async (req, res) => {
+	const { name, email, phone, type } = req.body;
+
+	// Build contact object
+	const contactFields = {};
+	if (name) contactFields.name = name;
+	if (email) contactFields.email = email;
+	if (phone) contactFields.phone = phone;
+	if (type) contactFields.type = type;
+
+	try {
+		let contact = await Contact.findById(req.params.id);
+		if (!contact) return res.status(401).json({ msg: 'Contact not found' });
+
+		// make sure user owns the contact
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Unauthorized action' });
+		}
+		contact = await Contact.findByIdAndUpdate(
+			req.params.id,
+			{ $set: contactFields },
+			{ $new: true }
+		);
+		res.json(contact);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route DELETE api/contacts/:id
 // @desc Delete a contact
 // @access Private
-router.delete('/:id', (req, res) => {
-	res.send('Delete  contact');
+router.delete('/:id', authCheck, async (req, res) => {
+	try {
+		let contact = await Contact.findById(req.params.id);
+		if (!contact) return res.status(401).json({ msg: 'Contact not found' });
+
+		// make sure user owns the contact
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Unauthorized action' });
+		}
+		await Contact.findByIdAndRemove(req.params.id);
+		res.json({ msg: 'Contact removed' });
+
+		res.json(contact);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 module.exports = router;
