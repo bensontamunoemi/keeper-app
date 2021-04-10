5fd63c9e4a64e7c9a846bd6eaa3c21570d43c06d
massive5566 <bensontamunoemi@gmail.com>
2021-04-10 21:01:54 +0400

    Ready for production setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 // Connect Database
 connectDB();
@@ -15,6 +16,16 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contact', require('./routes/contact'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 const port = process.env.PORT || 5000;
 
